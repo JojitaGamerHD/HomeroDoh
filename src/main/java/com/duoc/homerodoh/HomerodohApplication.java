f314a38b3503a1a49068e6cbf64db2bec14cb5ea
JoaquinCardenas99 <jo.cardenasb@duocuc.cl>
2026-04-13 09:24:49 -0400

    Add files via upload
    Spring Boot

---
+++ src/main/java/com/duoc/homerodoh/HomerodohApplication.java
@@ -0,0 +1,13 @@
+package com.duoc.homerodoh;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HomerodohApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HomerodohApplication.class, args);
+	}
+
+}
